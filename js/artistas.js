343df4b2317775706f2186bc251b07cf5dc32468
Clara Farias <138786154+clarafbr@users.noreply.github.com>
2024-02-28 11:22:54 -0300

    Third Commit

--- js/artistas.js
+++ js/artistas.js
@@ -1,29 +1,43 @@
 const btn = document.querySelector("#btn-artistas")
 
-btn.addEventListener('click', (event)=>{
+btn.addEventListener('click', () => {
     event.preventDefault() //torna o campo obrigatório
 
-        const personagem = document.getElementById('input-artistas').value
-        //valor do index do parâmetro
+    const personagem = document.getElementById('input-artistas').value
+    //valor do index do parâmetro
 
     const url = `https://www.vagalume.com.br/${personagem}/index.js`
-    console.log(url)
 
     fetch(url)
-    .then((response)=>{
-        if(!response.ok){
-            throw new Error('Erro de rede código '+response.status)
-        }
-        return response.json()
-    })
-    .then((data)=>{
-        personagem(data)
-    })
-    .catch((err)=>console.log(err))
-
-    // function personagem(item){
-
-    // }
-    
-
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Erro de rede código ' + response.status)
+            }
+            return response.json()
+        })
+        .then((data) => {
+            catchData(data)
+        })
+        .catch((err) => console.log(err))
 })
+
+
+
+function catchData(data) {
+    const resultList = document.querySelector('.results-list-artist')
+    const resultArtist = document.createElement('div')
+
+    resultArtist.innerHTML =
+        `
+    <div class="nome-img-artist">
+        <img class="img-artist" src="https://www.vagalume.com.br/${data.artist.pic_small}" alt="">
+        <h1>${data.artist.desc}</h1>
+    </div>
+    <div>
+    </div>
+    `
+
+
+    resultList.appendChild(resultArtist)
+
+}
